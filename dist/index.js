0ff7954ad7b5b6a8e5e1cd56b9b1e4437d7691aa
Cristian <ctomatfl@gmail.com>
2023-02-13 10:12:50 -0400

    Finish styles and sketch card creator

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+const cardsContainer = document.querySelector('.cards-container');
+const myLibrary = [];
+function Book(title, author, numberOfPages) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+}
+myLibrary.push(new Book('The Final Empire', 'Brandon Sanderson', 422));
+function createCardElement() {
+    const card = document.createElement('div');
+    const titleBook = document.createElement('p');
+    const authorBook = document.createElement('p');
+    const pagesBook = document.createElement('p');
+    const buttonGroup = document.createElement('div');
+    const readButton = document.createElement('button');
+    const removeButton = document.createElement('button');
+    card.classList.add('card-container');
+    buttonGroup.classList.add('button-group');
+    readButton.classList.add('btn');
+    removeButton.classList.add('btn');
+    readButton.setAttribute('id', 'read-button');
+    removeButton.setAttribute('id', 'remove-button');
+    titleBook.innerText = 'The Final Empire';
+    authorBook.innerText = 'Brandon Sanderson';
+    pagesBook.innerText = `${422} pages`;
+    removeButton.innerText = 'Remove';
+    readButton.innerText = 'Read';
+    buttonGroup.appendChild(readButton);
+    buttonGroup.appendChild(removeButton);
+    card.appendChild(titleBook);
+    card.appendChild(authorBook);
+    card.appendChild(pagesBook);
+    card.appendChild(buttonGroup);
+    cardsContainer.appendChild(card);
+}
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+createCardElement();
+export {};
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,52 @@
-function Book(
-  title: string,
-  author: string,
-  numberPages: number,
-  read: boolean
-) {
-  this.title = title
-  this.author = author
-  this.numberPages = numberPages
-  this.read = read
-  this.info = () =>
-    `${this.title}, ${this.author}, ${this.numberPages} pages, ${
-      this.read ? 'Readed' : 'No readed yet'
-    }`
-}
+const cardsContainer = document.querySelector('.cards-container')
 
-const redRising: {
+type bookType = {
   title: string
   author: string
-  numberPages: number
-  read: boolean
-  info: () => void
-} = new Book('Red Rising', 'Pierce Brown', 382, false)
+  numberOfPages: number
+}
 
-console.log(redRising.info())
+const myLibrary: bookType[] = []
+
+function Book(title: string, author: string, numberOfPages: number) {
+  this.title = title
+  this.author = author
+  this.numberOfPages = numberOfPages
+}
+
+myLibrary.push(new Book('The Final Empire', 'Brandon Sanderson', 422))
+
+function createCardElement() {
+  const card = document.createElement('div')
+  const titleBook = document.createElement('p')
+  const authorBook = document.createElement('p')
+  const pagesBook = document.createElement('p')
+  const buttonGroup = document.createElement('div')
+  const readButton = document.createElement('button')
+  const removeButton = document.createElement('button')
+
+  card.classList.add('card-container')
+  buttonGroup.classList.add('button-group')
+  readButton.classList.add('btn')
+  removeButton.classList.add('btn')
+
+  readButton.setAttribute('id', 'read-button')
+  removeButton.setAttribute('id', 'remove-button')
+
+  titleBook.innerText = 'The Final Empire'
+  authorBook.innerText = 'Brandon Sanderson'
+  pagesBook.innerText = `${422} pages`
+  removeButton.innerText = 'Remove'
+  readButton.innerText = 'Read'
+
+  buttonGroup.appendChild(readButton)
+  buttonGroup.appendChild(removeButton)
+  card.appendChild(titleBook)
+  card.appendChild(authorBook)
+  card.appendChild(pagesBook)
+  card.appendChild(buttonGroup)
+
+  cardsContainer.appendChild(card)
+}
+
+createCardElement()
